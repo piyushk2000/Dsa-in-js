4a79ff075e057bca1d2564e47f205b514ddb9be1
Piyush <piyush.kalyani7@gmail.com>
2024-06-09 19:48:35 +0530

    parcitve sheet added

---
+++ jsSyntaxPractice.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3]
+
+// for (item of arr){
+//     console.log(item)
+// }
+const arr2 = [4,5,6]
+
+console.log(arr+arr2)
---
+++ pySyntaxPractice.py
@@ -0,0 +1,7 @@
+a = [1,2,3]
+b = [4,5,6]
+
+# print(a+b)
+
+for item in a:
+    print(item)
