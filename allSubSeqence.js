44de6f795a1ad36e270aecb02302f3f965941023
Piyush Kalyani <piyush.kalyani7@gmail.com>
2024-08-24 16:50:16 +0530

    some sol

---
+++ allSubSeqence.js
@@ -0,0 +1,3 @@
+for(let ele of [1,3,4]){
+    console.log(ele)
+}
---
+++ baiscjs.js
@@ -0,0 +1,24 @@
+// const printNNumber = (n) => {
+//     if(n==0){
+//         return
+//     }
+    
+//     printNNumber(n-1)
+//     console.log(n)
+//     return
+// }
+
+// printNNumber(10)
+
+/////////////////////////////////////// febo
+
+// const febo = (n) => {
+//     if ( n == 0 || n ==1 ){
+//         return 1
+//     }
+//     console.log(n)
+//     return (n+febo(n-1))
+// }
+
+// console.log(febo(3))
+
